2db08686c950b6bd8aee07b5ad655c023fddd2c8
darshangit <darshans1989@gmail.com>
2018-08-08 21:45:59 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  scrollInto(element) {
+    console.log('ekement', element);
+    const e1 = document.getElementById(element);
+    e1.scrollIntoView({behavior: 'smooth'});
+  }
 }
