20760bc14a9c38607b7de5797d518a0bd087a2a7
darshangit <darshans1989@gmail.com>
2018-08-09 20:50:00 +0530

    added counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,10 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  counter = 0;
 
   scrollInto(element) {
+    this.counter = 0;
     console.log('ekement', element);
     const e1 = document.getElementById(element);
     e1.scrollIntoView({behavior: 'smooth'});
+
+
+    const source = interval(70);
+    const takeWh = source.pipe(takeWhile(() => this.counter <= 55));
+    takeWh.subscribe(val => this.counter = this.counter + 1);
+    }
   }
 }
