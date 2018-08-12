21cf229fdbcf28af0bb5a96130aa45c52c6d466c
darshangit <darshans1989@gmail.com>
2018-08-12 19:27:57 +0530

    tech + experience page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
-import { interval } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
-
+import { interval, of } from 'rxjs';
+import { takeWhile, mapTo, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +11,52 @@ import { takeWhile } from 'rxjs/operators';
 export class AppComponent {
   title = 'app';
   counter = 0;
+  experienceCounter = 0;
 
-  scrollInto(element) {
-    this.counter = 0;
-    console.log('ekement', element);
-    const e1 = document.getElementById(element);
-    e1.scrollIntoView({behavior: 'smooth'});
+  selectedElement: string = null;
+  techElementSelected: boolean;
+  expierienceElementSelected: boolean;
 
+  scrollInto = element => {
+    this.selectedElement = element;
+    this.techElementSelected = this.techElementSelected
+      ? true
+      : this.selectedElement === 'techClass';
+    this.expierienceElementSelected = this.expierienceElementSelected
+      ? true
+      : this.selectedElement === 'expClass';
 
-    const source = interval(70);
-    const takeWh = source.pipe(takeWhile(() => this.counter <= 55));
-    takeWh.subscribe(val => this.counter = this.counter + 1);
-    }
+    const incomingElementObservable = of(1);
+    const observablePiping = incomingElementObservable.pipe(delay(100));
+
+    observablePiping.subscribe(val => {
+      const e1 = document.getElementById(element);
+      e1.scrollIntoView({ behavior: 'smooth' });
+
+      if (element === 'techClass') {
+        this.gitProjectsIncrementor();
+      }
+
+      if (element === 'expClass') {
+        this.experienceCounterIncrementor();
+      }
+    });
   }
 
+  gitProjectsIncrementor = () => {
+    this.counter = 0;
+    const source = interval(70);
+    const takeWh = source.pipe(takeWhile(() => this.counter <= 55));
+    takeWh.subscribe(() => (this.counter = this.counter + 1));
+  }
+
+  experienceCounterIncrementor = () => {
+    this.experienceCounter = 0;
+    const source = interval(250);
+    const takeWh = source.pipe(takeWhile(() => this.experienceCounter < 7));
+    takeWh.subscribe(
+      () => (this.experienceCounter = this.experienceCounter + 1)
+    );
+  }
+
+}
